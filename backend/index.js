c5cb33696aa0785ecf0207d3e25ea8224a50bf0c
Abhinav.r.1111 <abhinav.r.1111@gmail.com>
2024-09-12 17:27:30 +0530

    ula la

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://nodemailer-3.onrender.com",  // Replace with your actual frontend URL
+    origin: "*",  // Replace with your actual frontend URL
     methods: "GET,POST,PUT,DELETE",  // Specify allowed HTTP methods
     credentials: true,  // If you are dealing with cookies or sessions
 }));
